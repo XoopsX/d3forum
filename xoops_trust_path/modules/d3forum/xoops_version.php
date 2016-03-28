244095ec2b38b8eb25983880400d64cc5589334b
nao-pon <hypweb@gmail.com>
2016-03-28 17:39:02 +0900

    d3forum version 0.89.5

--- xoops_trust_path/modules/d3forum/xoops_version.php
+++ xoops_trust_path/modules/d3forum/xoops_version.php
@@ -12,7 +12,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 $modversion['name'] = constant($constpref.'_NAME') ;
 $modversion['description'] = constant($constpref.'_DESC') ;
 $modversion['version'] = 0.89 ;
-$modversion['detailed_version'] = '0.89.4' ;
+$modversion['detailed_version'] = '0.89.5' ;
 $modversion['credits'] = "PEAK Corp. and JIDAIKOBO";
 $modversion['author'] = "GIJ=CHECKMATE and JIDAIKOBO and hackd naao, nao-pon, domifara" ;
 $modversion['help'] = "" ;
